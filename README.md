7ce78c47c06d97fca247ca4aa2489011fee112e8
Orlando Cardenas <orlando0992@gmail.com>
2024-05-31 01:30:03 -0400

    Subida de trabajo

---
+++ README.md
@@ -0,0 +1,4 @@
+Para acceder a la aplicación, se debe utilizar las credenciales de los usuarios que están predefinidos en el sistema. Estos usuarios están almacenados en el archivo `agentModel.js` del proyecto, donde cada usuario tiene un conjunto de credenciales como nombre, email y contraseña.
+
+
+
---
+++ src/models/agentModel.js
@@ -0,0 +1,24 @@
+export const agents = [
+    {
+        email: 'alba@fbi.com',
+        password: 'alba',
+        name: 'Albamar Flores De Castro',
+    },
+    {
+        email: 'who@fbi.com',
+        password: 'me',
+        name: 'John Doe',
+    },
+
+    {
+        email: 'where@fbi.com',
+        password: 'there',
+        name: 'Jane Doe',
+    },
+    {
+        email: 'how@fbi.com',
+        password: 'exactly',
+        name: 'Jim Doe',
+    },
+
+];
